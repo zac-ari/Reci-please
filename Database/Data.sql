ea6e23d72aabecea1e739bbae918baa935f5c26b
Zac <z.nicholls@live.com.au>
2023-05-22 21:53:49 +1000

    Refining

--- Database/Data.sql
+++ Database/Data.sql
@@ -14,8 +14,17 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Dumping data for table recipedatabase.user: ~1 rows (approximately)
-REPLACE INTO `user` (`UserID`, `Username`, `Email`, `Password`, `Fav1`, `Fav2`, `Fav3`, `Fav4`, `Fav5`, `Fav6`, `Fav7`, `Fav8`, `Fav9`) VALUES
+-- Dumping data for table recipedatabase.recipes: ~2 rows (approximately)
+REPLACE INTO `recipes` (`RecipeID`, `Title`, `Description`, `Ingredients`, `Method`) VALUES
+	(1, 'test', 'test', 'test', 'test'),
+	(2, 'test2', 'test2', 'test2', 'test2');
+
+-- Dumping data for table recipedatabase.reviews: ~1 rows (approximately)
+REPLACE INTO `reviews` (`ReviewID`, `RecipeID`, `UserID`, `Rating`, `Comments`) VALUES
+	(1, 1, 1, 5, 'was good test');
+
+-- Dumping data for table recipedatabase.users: ~1 rows (approximately)
+REPLACE INTO `users` (`UserID`, `Username`, `Email`, `Password`, `Fav1`, `Fav2`, `Fav3`, `Fav4`, `Fav5`, `Fav6`, `Fav7`, `Fav8`, `Fav9`) VALUES
 	(1, 'Zac', 'zac@gmail.com', 'Password', 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
